05660b53c2ce772dbed42cb9a71933defaeb2b9a
Vishnu Nanilal Panicker <vishnunlal@gmail.com>
2024-05-04 18:02:35 +0530

    Implemented user sign-in controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 async function register(req, res) {
     try {
@@ -45,6 +46,45 @@ async function register(req, res) {
     }
 }
 
+async function signIn(req, res) {
+    try {
+        let { email, phone_no, password } = req.body;
+        console.log("reached")
+        let emailResponse = await User.findOne({ email })
+        let phoneResponse = await User.findOne({ phone_no })
+        if (!emailResponse && !phoneResponse) {
+            res.status(404).send({
+                success: false,
+                message: "Wrong credentials."
+            })
+            return;
+        }
+
+        let response = emailResponse || phoneResponse;
+        
+        response = await bcrypt.compare(password, response.password)
+        if (response) {
+            //create jwt and send to front end
+            response = jwt.sign({credential: email||phone_no, password}, process.env.jwt_key)
+            res.status(201).send({
+                success: true,
+                message: "User signed in successfully.",
+                data: response
+            })
+        }
+        else {
+            res.status(404).send({
+                success: false,
+                message: "Wrong credentials.",
+            })
+        }
+    }
+    catch(err){
+        console.log("User registration failed on DB.")
+    }
+}
+
 module.exports = {
     register,
+    signIn
 }
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const userCtrl = require('../controllers/userController')
 
 router.post('/register', userCtrl.register)
-
-
+router.get('/sign-in', userCtrl.signIn)
+router.get()
 module.exports=router
